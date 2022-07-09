57083de3dfee8d08865a7ec208e7089c54c8efe8
waterbeen0530 <hange253@gmail.com>
2022-07-10 01:04:31 +0900

    [CREATE] Add MainItem.jsx
    MainItem.jsx 생성

---
+++ Components/MainPage/MainItem.jsx
@@ -0,0 +1,45 @@
+import styled from "@emotion/styled";
+
+export default function () {
+  return (
+    <Container>
+      <Title>
+        <h1>
+          ABOUT
+          <br />
+          COVID-19
+        </h1>
+        <p>간편하게 코로나19 확진자 통계 정보를 확인하세요!</p>
+      </Title>
+      <img src="/img/MainPage/mainIcon.png" alt="" />
+    </Container>
+  );
+}
+
+const Container = styled.div`
+  width: 100vw;
+  height: 100vh;
+  gap: 100px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+
+  img {
+    width: 570px;
+    height: auto;
+  }
+`;
+
+const Title = styled.div`
+  color: #252b59;
+  h1 {
+    margin: 0;
+    font-weight: 600;
+    font-size: 90px;
+  }
+  p {
+    margin: 20px 0 0;
+    font-weight: 600;
+    font-size: 25px;
+  }
+`;
