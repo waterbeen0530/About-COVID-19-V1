f2d8014a6c736dad00fe0f2a003e009888beeed2
waterbeen0530 <hange253@gmail.com>
2022-07-10 23:10:24 +0900

    [UPDATE] Main>Background 완성
    MainPage > Background 완성

--- Components/MainPage/Background.jsx
+++ Components/MainPage/Background.jsx
@@ -1,3 +1,29 @@
+import styled from "@emotion/styled";
+
 export default function () {
-  return <></>;
+  return (
+    <Container>
+      <img src="/img/MainPage/Background1.png" alt="" />
+      <img src="/img/MainPage/Background2.png" alt="" />
+    </Container>
+  );
 }
+
+const Container = styled.div`
+  & > img:first-child {
+    z-index: -1;
+    position: absolute;
+    top: 0;
+    right: 0;
+    width: 40%;
+    height: auto;
+  }
+  & > img:last-child {
+    z-index: -1;
+    position: absolute;
+    bottom: 0;
+    left: 0;
+    width: 40%;
+    height: auto;
+  }
+`;
