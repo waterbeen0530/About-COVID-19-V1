62ffe5bd914acbfdc6c75b931681f275215839c2
waterbeen0530 <hange253@gmail.com>
2022-07-10 23:08:43 +0900

    [UPDATE] Header 완성!
    Header 다 맹금

--- Components/common/Header.jsx
+++ Components/common/Header.jsx
@@ -1,7 +1,40 @@
+import styled from "@emotion/styled";
+
 export default function () {
   return (
-    <>
-      <></>
-    </>
+    <Conta>
+      <img src="/img/MainPage/logo.png" alt="" />
+      <ContentList>
+        <p>Introduction</p>
+        <p>Information</p>
+        <p>Portfolio</p>
+        <p>Developer</p>
+        <a href="https://url.kr/xrldu3">Arin</a>
+      </ContentList>
+    </Conta>
   );
 }
+
+const Conta = styled.div`
+  position: fixed;
+  width: 100%;
+  height: auto;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  background: #7988f4;
+  opacity: 0.9;
+  img {
+    width: 130px;
+    height: 40px;
+    margin-left: 25px;
+  }
+`;
+
+const ContentList = styled.nav`
+  margin-right: 25px;
+  gap: 60px;
+  display: flex;
+  align-items: center;
+  color: #fff;
+`;
