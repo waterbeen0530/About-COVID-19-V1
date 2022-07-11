e402ef056d54bcaea8441a917e470d21e9cb46d8
waterbeen0530 <hange253@gmail.com>
2022-07-11 23:35:33 +0900

    [REFACTORING] MainVeiw 경로 설정
    MainVeiw 경로 설정/세팅

--- Components/MainPage/MainVeiw.jsx
+++ Components/MainPage/MainVeiw.jsx
@@ -1,9 +1,18 @@
+import styled from "@emotion/styled";
 import Header from "../common/Header";
+import MainItem from "./MainItem";
 
 export default function () {
   return (
-    <>
+    <Container>
       <Header />
-    </>
+      <MainItem />
+    </Container>
   );
 }
+
+const Container = styled.div`
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+`;
