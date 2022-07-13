520a1657d151b4de0b416d694dfbe0ad0dce7ad2
waterbeen0530 <hange253@gmail.com>
2022-07-13 23:36:15 +0900

    [SETTING] file
    ^_^

--- next.config.js
+++ next.config.js
@@ -1,6 +1,13 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-}
+};
 
-module.exports = nextConfig
+const withImages = require("next-images");
+module.exports = withImages({
+  webpack(config, options) {
+    return config;
+  },
+});
+
+module.exports = nextConfig;
