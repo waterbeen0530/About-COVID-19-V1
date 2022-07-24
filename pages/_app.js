e993a718a21b51d295f00538fbaf6d12a18bcfae
waterbeen0530 <hange253@gmail.com>
2022-07-24 22:39:38 +0900

    [REFACTORING] _app.js 경로 설정
    .

--- pages/_app.js
+++ pages/_app.js
@@ -1,12 +1,8 @@
 import "../styles/globals.css";
-import MainVeiw from "../Components/MainPage/MainVeiw";
-import InfoVeiw from "../Components/InformationPage/InfoVeiw";
 
 function MyApp({ Component, pageProps }) {
   return (
     <>
-      <MainVeiw />
-      <InfoVeiw />
       <Component {...pageProps} />
     </>
   );
--- pages/index.js
+++ pages/index.js
@@ -1,3 +1,17 @@
+import MainVeiw from "../Components/MainPage/MainVeiw";
+import InfoVeiw from "../Components/InformationPage/InfoVeiw";
+import PortVeiw from "../Components/PortfolioPage/PortVeiw";
+import Footer from "../Components/common/Footer";
+import KorAllVeiw from "../Components/KorAllPAge/KorAllVeiw";
+
 export default function () {
-  return <></>;
+  return (
+    <>
+      <MainVeiw />
+      <InfoVeiw />
+      <PortVeiw />
+      <Footer />
+      <KorAllVeiw />
+    </>
+  );
 }
